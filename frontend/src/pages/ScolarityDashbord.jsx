0d067213f807251f2168c8d9b1b50b8027d25d78
jamigit <ingmicheljasmin@hotmail.fr>
2025-12-23 09:41:46 -0500

    Add Student and Scolarity Dashbord

---
+++ frontend/src/pages/ScolarityDashbord.jsx
@@ -0,0 +1,11 @@
+import React from 'react';  
+
+const ScolarityDashboard = () => {
+  return (
+    <div>
+      <h1>Scolarity Dashboard Page</h1>
+    </div>
+  );
+};
+
+export default ScolarityDashboard; 
