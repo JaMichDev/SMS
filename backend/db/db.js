8c2f71653844972857041a2669e890e388fccafd
jamigit <ingmicheljasmin@hotmail.fr>
2025-12-23 06:18:02 -0500

    Update in String connection

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,8 +1,10 @@
-import mongoose from 'mongoose'; //mongoose : ORM pour MongoDB
+import mongoose from "mongoose";               //mongoose : ORM pour MongoDB
 
 const connectToDatabase = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI);//Connexion à la base de données MongoDB    
+    const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pviohoc.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+    await mongoose.connect(uri);
     console.log('MongoDB connected successfully');
   } catch (error) {
     console.error('MongoDB connection error:', error);
@@ -11,3 +13,5 @@ const connectToDatabase = async () => {
 };
 
 export default connectToDatabase;
+
+
