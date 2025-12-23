64674edb9c26a8a35bdaff4a6cac283e07b62ecb
jamigit <ingmicheljasmin@hotmail.fr>
2025-12-23 05:56:52 -0500

    Insert many user

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,16 +1,20 @@
 import mongoose from "mongoose"; //mongoose : ORM pour MongoDB
 
 //Ce code sert à définir le modèle User (Utilisateur) dans MongoDB à l’aide de Mongoose.
-const userSchema = new mongoose.Schema({
-    name: { type: String, required: true},
-    email: { type: String, required: true },
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    role: { type: String, enum: ['admin', 'scolarite', 'student'], required: true },
-    profileImage: { type: String }, // URL de l'image de profil
-    createAt: { type: Date, default: Date.now }, // Date de création de l'utilisateur    
-    updatedAt: { type: Date, default: Date.now } // Date de la dernière mise à jour de l'utilisateur    
-});
+    role: {
+      type: String,
+      enum: ['admin', 'scolarite', 'student'],
+      required: true
+    },
+    profileImage: String
+  },
+  { timestamps: true }
+);
 
 const User = mongoose.model('User', userSchema);
-
 export default User;
--- backend/userSeed.js
+++ backend/userSeed.js
@@ -2,28 +2,55 @@ import User from './models/User.js';
 import bcrypt from 'bcryptjs';
 import connectToDatabase from './db/db.js';
 
+import mongoose from "mongoose"; //mongoose : ORM pour MongoDB
+
+//Ce code sert à définir le modèle User (Utilisateur) dans MongoDB à l’aide de Mongoose.
+/*const userSchema = new mongoose.Schema({
+    name: { type: String, required: true},
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    role: { type: String, enum: ['admin', 'scolarite', 'student'], required: true },
+    profileImage: { type: String }, // URL de l'image de profil
+    createAt: { type: Date, default: Date.now }, // Date de création de l'utilisateur    
+    updatedAt: { type: Date, default: Date.now } // Date de la dernière mise à jour de l'utilisateur    
+});
+*/
+
 const userRegister = async () => {
   try {
     await connectToDatabase();
 
     const hashedPassword = await bcrypt.hash('admin123', 10);
 
-    const adminUser = new User({
-      name: 'Admin',
-      email: 'admin@example.com',
-      password: hashedPassword,
-      role: 'admin'
-    });
+    const users = [
+      {
+        name: 'Admin',
+        email: 'admin@example.com',
+        password: hashedPassword,
+        role: 'admin'
+      },
+      {
+        name: 'Scolarity',
+        email: 'scolarity@example.com',
+        password: hashedPassword,
+        role: 'scolarite'
+      },
+      {
+        name: 'Student',
+        email: 'student@example.com',
+        password: hashedPassword,
+        role: 'student'
+      }
+    ];
 
-    await adminUser.save();
-    console.log('Admin user seeded successfully');
+    await User.insertMany(users);
 
+    console.log('Users seeded successfully');
     process.exit(0);
   } catch (error) {
-    console.error('Error seeding admin user:', error);
+    console.error('Error seeding users:', error);
     process.exit(1);
   }
 };
 
 userRegister();
-                 
