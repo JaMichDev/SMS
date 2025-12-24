be1dad9cad4de8592014d2fb1e19c3cc5243c410
jamigit <ingmicheljasmin@hotmail.fr>
2025-12-23 23:11:06 -0500

    Update login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -24,14 +24,31 @@ export default function Login() {
   };
 
   return (
-    <div>
-      <h2>Login</h2>
+    <div className="flex flex-col items-center justify-center h-screen bg-gradient-to-b from-teal-600 from-50% to gray-100 to-50% space-y-6 ">
+      
+      <h2 className="font-bold text-3xl ">Student Management System</h2>
 
       {/* Login local */}
       <form>
-        <input placeholder="Username" />
-        <input type="password" placeholder="Password" />
-        <button type="button">Login</button>
+              <div className="mb-4">Local Login
+                 <label htmlFor='email' className="block text-gray-700">Email</label> 
+                 <input onChange={(e)=>setEmail(e.target.value)} type="email" placeholder="Username or Email" className="w-full px-3 py-2 border"/>
+              </div> 
+              <div className="mb-4">
+                  <label htmlFor='password' className="block text-gray-700">Password</label> 
+                  <input type="password" placeholder="******" className="w-full px-3 py-2 border"/>
+               </div>  
+               <div className="mb-4 flex items-center justify-between">
+                  <label className="inline-flex items-center">
+                     <input type="checkbox" className="form-checkbox"/>
+                     <span className="ml-2 text-gray-700">  Remember me</span>  
+                  </label>
+                  <a href='#' className="text-sm text-blue-600 hover:underline"> Forgot Password?</a> 
+
+               </div>
+                <div className="mb-4">
+                    <button type="submit" className="w-full bg-teal-600 text-white py-2">Login</button>  
+               </div>
       </form>
 
       <hr />
